4bf8b6ec37b291c79c200d69a445b4a147681a4b
d-kaki <dkaki77@gmail.com>
2023-03-17 04:02:55 +0100

    added Task 14 17 and 18 files

---
+++ Task_14&17&18/Task14FirstJavaProjectFiles/src/Main.java
@@ -0,0 +1,19 @@
+    public class Main {
+        static {
+            System.out.println("FIRST JAVA PROJECT");
+            System.out.println("Written by David Kaki");
+            System.out.println("January 2023 Cohort");
+        }
+        public static void main(String[] args) {
+            System.out.println("Hello world. This is my First Project!");
+            System.out.println("My name is David Kaki");
+            System.out.println("I am from Benue State and reside in Makurdi. I am a medical Physicist switching to Software testing via Testify Academy");
+            System.out.println("As a medical Physicist, I use my knowledge of physics to help diagnose and treat patients.");
+            System.out.println("I use radiation and imaging equipment to create images of the body and analyze them.");
+            System.out.println("I collaborates with doctors and other medical professionals to determine the best course of treatment for patients.");
+            System.out.println("I enjoy working in the healthcare field because I feel like I am making a difference in people's lives.");
+            System.out.println("In my free time, I enjoy spending time with his family.");
+            System.out.println("Tech is my new hobby.");
+            System.out.println("I am grateful for the opportunity to develop and build.");
+        }
+    }
